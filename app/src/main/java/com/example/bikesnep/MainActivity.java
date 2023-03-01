d88f5d73fefba82fdadd74ef0ccdecd2f849d780
Sabin77 <sabinlc47@gmail.com>
2023-03-01 09:33:46 +0545

    first

---
+++ app/src/main/java/com/example/bikesnep/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.bikesnep;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rcv;
+    myadapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("BikesNepal");
+
+        rcv = (RecyclerView) findViewById(R.id.recview);
+        //  rcv.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new myadapter(dataqueue(),getApplicationContext());
+        rcv.setAdapter(adapter);
+
+        //LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        //rcv.setLayoutManager(layoutManager);
+
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
+        rcv.setLayoutManager(gridLayoutManager);
+    }
+
+    public ArrayList<Model> dataqueue()
+    {
+        ArrayList<Model> holder=new ArrayList<>();
+
+        Model ob1=new Model();
+        ob1.setHeader("TVS");
+        ob1.setImgname(R.drawable.tv3);
+        holder.add(ob1);
+
+        Model ob2=new Model();
+        ob2.setHeader("Bajaj");
+        ob2.setImgname(R.drawable.bajaj1);
+        holder.add(ob2);
+
+        Model ob3=new Model();
+        ob3.setHeader("Yamaha");
+        ob3.setImgname(R.drawable.yamaha2);
+        holder.add(ob3);
+
+        Model ob4=new Model();
+        ob4.setHeader("KTM");
+        ob4.setImgname(R.drawable.ktm1);
+        holder.add(ob4);
+
+        Model ob5=new Model();
+        ob5.setHeader("Suzuki");
+        ob5.setImgname(R.drawable.suzuki1);
+        holder.add(ob5);
+
+        Model ob6=new Model();
+        ob6.setHeader("Royal Enfield");
+        ob6.setImgname(R.drawable.royalenfield1);
+        holder.add(ob6);
+
+        Model ob7=new Model();
+        ob7.setHeader("Magento Framework");
+        ob7.setDesc("PHP Based e-Comm Framework");
+        ob7.setImgname(R.drawable.tv3);
+        holder.add(ob7);
+
+        Model ob8=new Model();
+        ob8.setHeader("Shopify Framework");
+        ob8.setDesc("PHP based e-Comm Framework");
+        ob8.setImgname(R.drawable.bajaj1);
+        holder.add(ob8);
+
+        Model ob9=new Model();
+        ob9.setHeader("Angular Programming");
+        ob9.setDesc("Web Programming");
+        ob9.setImgname(R.drawable.ktm1);
+        holder.add(ob9);
+
+        Model ob10=new Model();
+        ob10.setHeader("Python Programming");
+        ob10.setDesc("Desktop/Web based Progamming");
+        ob10.setImgname(R.drawable.royalenfield1);
+        holder.add(ob10);
+
+        Model ob11=new Model();
+        ob11.setHeader("Node JS Programming");
+        ob11.setDesc("Web based Programming");
+        ob11.setImgname(R.drawable.suzuki1);
+        holder.add(ob11);
+
+
+        return holder;
+    }
+
+
+}
